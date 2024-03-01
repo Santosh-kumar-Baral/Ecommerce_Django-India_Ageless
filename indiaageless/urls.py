2c6f50cbe8a14e1f517eda4949edcebdacafb780
Santosh Kumar Baral <120707301+Santosh-kumar-Baral@users.noreply.github.com>
2024-03-01 11:55:27 +0530

    Add files via upload

---
+++ indiaageless/urls.py
@@ -0,0 +1,34 @@
+"""
+URL configuration for indiaageless project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from mainapp.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',main,name="index"),
+    path('wishlist/', wishlist, name='wishlist'),
+    path('contact/', contact, name='contact'),
+    path('about-us/', about, name='about-us'),
+    path('checkout/', checkout, name='checkout'),
+    path('kannauj perfume/', product, name='kannauj perfume'),
+    path('faq/', faq, name='faq'),
+    path('shopping_cart/', cart, name='shopping cart'),
+    path('login register/', register, name='login register'),
+    path('login/', login_view, name='login'),
+    path('register/', register_view, name='register')
+]
